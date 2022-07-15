a091fc51ce44fc743b65cd54af6af8bf033e9d28
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-07-15 13:23:04 -0400

    Where I left off before lunch. So this can be seen on the issue.

--- controller/puzzle_routes.js
+++ controller/puzzle_routes.js
@@ -4,17 +4,17 @@ const Puzzle = require('../models/puzzle')
 const Collection = require('../models/collection')
 
 // DELETE - Delete
-router.delete('/delete/:id', (req, res) => {
-    const PuzzleId = req.params.id
+// router.delete('/delete/:id', (req, res) => {
+//     const PuzzleId = req.params.id
 
-    Puzzle.findByIdAndRemove(PuzzleId)
-        .then(puzzle => {
-            res.redirect('user')
-        })
-        .catch(err => {
-            res.json(err)
-        })
-})
+//     Puzzle.findByIdAndRemove(PuzzleId)
+//         .then(puzzle => {
+//             res.redirect('user')
+//         })
+//         .catch(err => {
+//             res.json(err)
+//         })
+// })
 
 // GET
 router.get('/:collectionId/new', (req, res) => {
@@ -27,32 +27,30 @@ router.get('/:collectionId/new', (req, res) => {
 
 // POST - Create
 router.post('/', (req, res) => {
-    console.log('res.body: '+req.body)
-    // const collectionId = req.body.collections
-    // console.log('collectionId: '+collectionId)
+    // console.log('HI')
+    const collectionId = req.body.collections
+    console.log('collectionId: '+collectionId)
     
-    // req.body.owner = req.session.userId
-    // req.body.public = req.body.public === 'on'
-    // // req.body.collections.push(collectionId)
-
-    // console.log('res.body: '+req.body)
-    // Puzzle.create(req.body)
-    //     .then(puzzle => {
-    //         console.log('Puzzle object created: '+puzzle)
-    //         .then(puzzle => {
-    //             console.log('returned puzzle in the next .then '+puzzle )
-    //             Collection.findById(collectionId)
-    //                 .then(collection => {
-    //                     collection.puzzle = puzzle
-    //                     console.log('collection that puzzle should be added to '+collection)
-    //                     res.redirect(`/collection/${req.body.collection}`)
-    //                 })
-    //             // res.json(puzzle)
-    //         }).catch(err => console.error)
-    //     })
-    //     .catch(err => console.error)
-        
-     
+    req.body.owner = req.session.userId
+    req.body.public = req.body.public === 'on'
+    
+    console.log('res.body: '+JSON.stringify(req.body))
+    Puzzle.create(req.body)
+        .then(puzzle => {
+                console.log('Puzzle object created: '+puzzle)
+                .then(puzzle => {
+                    console.log('returned puzzle in the next .then '+puzzle )
+                    Collection.findById(collectionId)
+                        .then(collection => {
+                            collection.puzzle = puzzle
+                            console.log('collection that puzzle should be added to '+collection)
+                            res.redirect(`/collection/${req.body.collection}`)
+                        })
+                        .catch(err => res.send(err))
+        })
+        .catch(err => res.send(err))
+    })
+    .catch(err => res.send(err))
 })
 
 module.exports = router
