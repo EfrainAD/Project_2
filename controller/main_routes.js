a8ef71bdf52afd0e7348c44b1eab4ea186868de8
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-07-21 00:18:26 -0400

    When click you gave it right it will up the day jumber by one, then move the duedate by that many days.

--- controller/main_routes.js
+++ controller/main_routes.js
@@ -21,7 +21,14 @@ router.get('/go', async (req, res) => {
 })
 
 router.get('/:id/right', async (req, res) => {
-     const tracker = await PersonalTracker.findById(req.params.id)
+     const id = req.params.id
+     const tracker = await PersonalTracker.findById(id)
+
+     tracker.dayJumper++ 
+     tracker.dueDate = new Date(+new Date(tracker.dueDate) + tracker.dayJumper*24*60*60*1000)
+     tracker.save();
+
+     res.redirect('/main/go')
 })
 
 router.get('/:id/wrong', async (req, res) => {
@@ -30,18 +37,9 @@ router.get('/:id/wrong', async (req, res) => {
      const id = req.params.id
      const tracker = await PersonalTracker.findById(id)
 
-     // nowDate = new Date.now()
-     // inTen = nowDate.setDate(nowDate.getMinutes()+10)  
-     
-     // console.log('HI, in /wrong nowDate is ', nowDate)
-     // console.log('HI, in /wrong nowDate is ', inTen)
-     // console.log('HI, id before edit in /wrong ', id)
-     console.log('personalTracker before edit in /wrong ', tracker)
-     // tracker.dueDate = new Date( (Date.now()).getTime() + 10*60000);
-     // tracker.dueDate = new Date(Date.now() + 10*60000);
-     tracker.dueDate = Date.now() + 10*60000
+     // console.log('personalTracker before edit in /wrong ', tracker)
+     tracker.dueDate = Date.now() + (10*60000)
      tracker.dayJumper = 0
-     // I think I need this but I think .then not. Need test.
      tracker.save();
      console.log('personalTracker after edit in /wrong ', tracker)
 
